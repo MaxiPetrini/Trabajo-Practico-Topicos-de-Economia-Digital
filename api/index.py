98c15d99f30c12d8de498eb15dad33215d0a52fa
MaxiPetrini <petrini715@gmail.com>
2026-05-12 02:03:33 -0300

    upgrade 6

--- api/index.py
+++ api/index.py
@@ -1,6 +1,3 @@
-from .main_code import RequestHandler
+from api.main_code import RequestHandler
 
-# Vercel busca cualquiera de estas tres; definimos todas para asegurar
 handler = RequestHandler
-app = RequestHandler
-application = RequestHandler
